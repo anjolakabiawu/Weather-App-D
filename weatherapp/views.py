179048abe1d1cf320d4fea5c9e58c6a09cc40c5a
Anjola <anjolakabiawu@yahoo.com>
2025-05-17 13:07:40 +0100

    Working on the page

--- weatherapp/views.py
+++ weatherapp/views.py
@@ -13,11 +13,11 @@ def home(request):
     url = f'https://api.openweathermap.org/data/2.5/weather?q={city}&appid=bf4e4ec77bd2a64b75f5b480321d54f3'
     PARAMS = {'units':'metric'}
 
-    data = request.get(url, PARAMS).json()
+    data = requests.get(url, PARAMS).json()
 
     description = data['weather'][0]['description']
     icon = data['weather'][0]['icon']
-    temp = data['main'][0]['temp']
+    temp = data['main']['temp']
 
     day = datetime.date.today()
-    return render(request, 'weatherapp/index.html', {'description': description, 'icon':icon, 'temp':temp, 'day':day})
+    return render(request, 'weatherapp/index.html', {'description': description, 'icon':icon, 'temp':temp, 'day':day, 'city':city})
